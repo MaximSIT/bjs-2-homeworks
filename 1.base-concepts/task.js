f7116012388daff37d97eb14c871ff47ff78857e
Maxim <max_dancer94@mail.ru>
2023-07-31 22:05:59 +0500

    Complete task 1. Ver.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+  d = b**2 - 4 * a * c;
+  if (d < 0) {
+    let arr = [];
+  } else if (d = 0) {
+    let x = -b / (2 * a);
+    let arr = [x];
+  } else (d > 0) {
+    let x1 = (-b + Math.sqrt(d) ) / (2*a);
+    let x2 = (-b - Math.sqrt(d) ) / (2*a);
+    let arr = [x1, x2];
+  }
   return arr;
 }
 

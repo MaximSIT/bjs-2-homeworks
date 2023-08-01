23bc4c0e2101c9d1f5396c6d14dff71bcf616241
Maxim <max_dancer94@mail.ru>
2023-08-01 17:40:14 +0500

    Complete task 1. Ver.3

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
-  d = b**2 - 4 * a * c;
+  let d = (b ** 2) - (4 * a * c);
+  let arr = [];
   if (d < 0) {
-    let arr = [];
-  } else if (d = 0) {
+    arr = [];
+  } else if (d === 0) {
     let x = -b / (2 * a);
-    let arr = [x];
+    arr = [x];
   } else {
     let x1 = (-b + Math.sqrt(d) ) / (2*a);
     let x2 = (-b - Math.sqrt(d) ) / (2*a);
-    let arr = [x1, x2];
+    arr = [x1, x2];
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let creditBody = amount - contribution;
+  let percentPerMonth = percent / 100 / 12;
+  let monthPayment = creditBody * (percentPerMonth + (percentPerMonth / (((1 + percentPerMonth) ** countMonths) - 1)));
+  let finalAmount = Number((monthPayment * countMonths).toFixed(2));
+  return finalAmount;
 }
